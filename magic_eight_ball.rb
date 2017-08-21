3f5908d7541c73a712b3674bb38f3a01eff114d3
Robert Scarlett <scarlettt10@msn.com>
2017-08-21 09:16:04 -0600

    place holder file with comments

---
+++ magic_eight_ball.rb
@@ -0,0 +1,7 @@
+
+=begin
+this is a place holder file, i was unsure how to do this project and looked for examples online
+i found a working magic eight ball, but now i can't think of a way of writing this without it appearing, 
+to completely ripping off someone elses work
+
+=end
